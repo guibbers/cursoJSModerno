7f3bf08badd324900a5e773c8450766b0a44b3b3
Guilherme Torres <guiatorres8@gmail.com>
2023-03-04 02:21:37 -0300

    node 02 done

---
+++ node/aula02/app.js
@@ -0,0 +1,8 @@
+const Cachorro = require('./b/c/d/mod');
+const path = require('path')
+
+const cachorrinho = new Cachorro('Sputnik Rodrigo');
+
+// cachorrinho.latir();
+
+console.log(path.resolve(__dirname, '..', '..', 'arquivos', 'imagens'));
